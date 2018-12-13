bc8cdbb44e8876a9281e7f3dd8432866ce0167a9
Pieter Desseyn <Pieter.Desseyn@codit.eu>
2018-12-13 16:39:34 +0100

    changed css to sass

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular7-Boilerplate'`, () => {
+  it(`should have as title 'AngularBoilerplate'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Angular7-Boilerplate');
+    expect(app.title).toEqual('AngularBoilerplate');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular7-Boilerplate!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to AngularBoilerplate!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular7-Boilerplate';
+  title = 'AngularBoilerplate';
 }
