a3aa586aa7520bca44a2a6a8fd254c3da809b643
Pieter Desseyn <Pieter.Desseyn@codit.eu>
2018-12-20 16:56:11 +0100

    cleaned up code

--- frontend/src/app/components/hero-detail/hero-detail.component.ts
+++ frontend/src/app/components/hero-detail/hero-detail.component.ts
@@ -31,9 +31,4 @@ export class HeroDetailComponent implements OnInit {
   goBack(): void {
     this.location.back();
   }
-
-  save(): void {
-    this.heroService.updateHero(this.hero)
-      .subscribe(() => this.goBack());
-  }
 }
