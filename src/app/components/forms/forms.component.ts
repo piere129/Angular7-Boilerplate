fb4ab2172891990d3f934efc8df28c36b5d2de2a
Pieter Desseyn <Pieter.Desseyn@codit.eu>
2020-01-22 08:45:05 +0100

    added better syntax template driven form

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -9,14 +9,22 @@ export class FormsComponent implements OnInit {
 
   constructor() { }
 
+  powers = ['Really Smart', 'Super Flexible',
+            'Super Hot', 'Weather Changer'];
+
   user = {
-    name: ''
+    name: '',
+    power: ''
   }
 
+
+// controls is an array in the ngForm, with key names equal to the 'name' attribute on the input
   onSubmit(e) {
     console.log(e.controls['username'].value);
     console.log(this.user.name);
     console.log(e.controls['username'].status);
+    console.log(this.user.power);
+    console.log(e.controls['power'].value);
   }
 
   ngOnInit() {
